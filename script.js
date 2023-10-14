2f21d53139bddb1142603972280154fded7d7de4
Terryphua <phuachenghongterry@gmail.com>
2023-10-14 11:09:22 +0800

    Added new stuff to all scrpt
    Added new stuff to all scrpt

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-function slideCard(cardElement) {
-    if (cardElement.style.transform === 'scale(1.1)') {
-        cardElement.style.transform = 'scale(1)';
-    } else {
-        cardElement.style.transform = 'scale(1.1)';
-    }
-}
-
-//For smooth transition when is nav bar is clicked.
 function smoothScroll(targetId) {
     event.preventDefault();
     const target = document.getElementById(targetId);
     target.scrollIntoView({
-      behavior: 'smooth'
-          });
-      }
+        behavior: 'smooth'
+    });
+}
+
+function smoothScroll(targetId) {
+    event.preventDefault();
+    const target = document.getElementById(targetId);
+    target.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
